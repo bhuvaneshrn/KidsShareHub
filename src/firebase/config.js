036b6367ab842f1e41672a583fac4297ea54acae
Bhuvanesh RN <bhuvaneshnadar2006@gmail.com>
2026-02-24 00:09:53 +0530

    Initial commit - Prototype version

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,19 +1,8 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-
 const firebaseConfig = {
-  apiKey: "AIzaSyCkdyf4akyNkna8LhuVHgGi37W4x8IKqtw",
-  authDomain: "kidshare-hub.firebaseapp.com",
-  projectId: "kidshare-hub",
-  storageBucket: "kidshare-hub.firebasestorage.app",
-  messagingSenderId: "770295938942",
-  appId: "1:770295938942:web:c8edfffd67760a712a3d2f"
+  apiKey:            process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain:        process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId:         process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket:     process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId:             process.env.REACT_APP_FIREBASE_APP_ID,
 };
-
-const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-
-export default app;
